d4858f45f80c5628094b618454cbe39aab115aa6
hi-artem <aakatev@virtru.com>
2020-10-10 14:26:09 -0700

    Added size limit for webpack config in to remove warning

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  performance: {
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000
+  },
   devServer: {
     publicPath: '/public/',
     compress: true,
